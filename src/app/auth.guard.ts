3d3f829d37234680d8177f4a56767ebf800f6626
safwen <asafouene@gmail.com>
2022-01-12 13:01:41 +0100

    commit

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { AuthentificationService } from 'src/app/_services/authentification.service';
-
+import { AuthentificationService } from './_services/authentification.service';
 @Injectable({
   providedIn: 'root'
 })
