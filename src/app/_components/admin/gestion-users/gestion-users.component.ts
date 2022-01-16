83b0188ae76e7cad310dad9eb50fa8c7309ddf72
safwen <asafouene@gmail.com>
2022-01-16 03:38:15 +0100

    commit

--- src/app/_components/admin/gestion-users/gestion-users.component.ts
+++ src/app/_components/admin/gestion-users/gestion-users.component.ts
@@ -23,14 +23,6 @@ export class GestionUsersComponent implements OnInit {
   constructor(private authService:AuthentificationService,private http:HttpClient) { 
     this.authService.OnGetUser().then((data)=>{
       this.users=data
-    }).then(()=>{
-      for (let i = 0; i < this.users.length; i++) {
-        if(this.users[i].Role==1)this.users[i].role="Administrateur"
-        else if (this.users[i].Role==0)this.users[i].role="Client"
-        if(this.users[i].url_img==undefined){
-          this.users[i].url_img="https://www.seekpng.com/png/detail/413-4139803_unknown-profile-profile-picture-unknown.png"
-        }
-      }
     }).then(()=>{this.loadPage=true})
     
   }
@@ -53,10 +45,12 @@ export class GestionUsersComponent implements OnInit {
 
   }
   onSave(id:any,f:any){
-    this.http.put("http://localhost:3000/users"+id,f).subscribe(()=>{
-      this.loadPage=false
-    }).closed
-    if(closed)this.loadPage=true
+    this.http.patch("http://localhost:3000/users/"+id,f).subscribe((res)=>{
+      this.authService.OnGetUser().then((data)=>{
+        this.users=data
+      }).then(()=>{this.loadPage=true})
+      this.authService.onSuccessNotif(true,"Utilisateur modifier avec success")
+    })
 
   }
   onAdd(f:any){
