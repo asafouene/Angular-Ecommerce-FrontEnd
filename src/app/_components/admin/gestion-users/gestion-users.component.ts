0b8225f334e9cff77fa5e55bf458f39b6373b3a4
safwen <asafouene@gmail.com>
2022-01-16 03:53:40 +0100

    commit

--- src/app/_components/admin/gestion-users/gestion-users.component.ts
+++ src/app/_components/admin/gestion-users/gestion-users.component.ts
@@ -24,16 +24,14 @@ export class GestionUsersComponent implements OnInit {
     this.authService.OnGetUser().then((data)=>{
       this.users=data
     }).then(()=>{this.loadPage=true})
-    
   }
 
   ngOnInit(): void {
   }
   onEdit(user:any){
     this.userEdit=user
-    
-
   }
+
   onDelate(user:any){
     if(user.role==1){
       let nbrAdmin=0
@@ -42,23 +40,35 @@ export class GestionUsersComponent implements OnInit {
      }
      if(nbrAdmin<2)this.authService.onAlertNotif(true,"Impossible car vous êtes le seul administrateur !")
     }
-
+    else{
+      this.http.delete("http://localhost:3000/users/"+user.id).subscribe((data)=>{        
+        this.authService.OnGetUser().then((data)=>{
+          this.users=data
+        }).then(()=>{this.loadPage=true})
+        this.authService.onSuccessNotif(true,"Utilisateur supprimer avec success")
+      })
+    }
   }
+
   onSave(id:any,f:any){
-    this.http.patch("http://localhost:3000/users/"+id,f).subscribe((res)=>{
+    this.http.patch("http://localhost:3000/users/"+id,f).subscribe(()=>{
       this.authService.OnGetUser().then((data)=>{
         this.users=data
       }).then(()=>{this.loadPage=true})
       this.authService.onSuccessNotif(true,"Utilisateur modifier avec success")
     })
+  }
 
-  }
   onAdd(f:any){
-    console.log(f);
-    
+    this.http.post("http://localhost:3000/users",f).subscribe((data)=>{
+      this.authService.OnGetUser().then((data)=>{
+        this.users=data
+        }).then(()=>{this.loadPage=true})
+        this.authService.onSuccessNotif(true,"Utilisateur ajouter avec success")    
+    }) 
   }
+
   onCancel(f:NgForm){
     f.reset()
   }
-
 }
